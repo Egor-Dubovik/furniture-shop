c68f6eac6c40c883cf6c4f491db72130df23f9d4
Egor Dubovik <egorka54896@gmail.com>
2022-06-26 23:20:15 +0300

    create function 'documentActions'

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+import { removeClasses } from "./functions.js";
+
+window.onload = () => {
+  const documentActions = (e) => {
+    const targetElement = e.target;
+
+    if (window.innerWidth > 768 && isMobile.any()) {
+      if (targetElement.classList.contains("menu__arrow")) {
+        targetElement.closest(".menu__item").classList.toggle("menu__item_hover");
+      }
+
+      const itemsHover = document.querySelectorAll(".menu__item.menu__item_hover");
+      if (!targetElement.closest(".menu__item") && itemsHover.length > 0) {
+        removeClasses(itemsHover, "menu__item_hover");
+      }
+    }
+  }
+
+  document.addEventListener("click", documentActions);
+}
